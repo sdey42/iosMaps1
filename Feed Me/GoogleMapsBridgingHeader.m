fb84530a55b4c6bbdd86fd09fee5089644f60a80
sila <siladitya.dey@gmail.com>
2015-10-04 17:31:53 -0700

    Done with creating Bridging Header file

---
+++ Feed Me/GoogleMapsBridgingHeader.m
@@ -0,0 +1,9 @@
+//
+//  GoogleMapsBridgingHeader.m
+//  Feed Me
+//
+//  Created by Siladitya on 10/4/15.
+//  Copyright (c) 2015 Ron Kliffer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
--- Podfile
+++ Podfile
@@ -4,5 +4,6 @@ use_frameworks!
 
 target 'Feed Me' do
 pod 'SwiftyJSON', '~> 2.2.0'
+pod 'GoogleMaps'
 end
 
